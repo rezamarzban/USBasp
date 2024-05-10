6e2c573dae7db37f403a0e1e62f4a45f810d448c
rezamarzban <73051714+rezamarzban@users.noreply.github.com>
2024-05-10 10:56:25 +0330

    Update main.c

--- samples/sinewave/main.c
+++ samples/sinewave/main.c
@@ -3,6 +3,10 @@
 
 #define PWM_PIN PB0 // PWM pin
 
+// Semi-sine wave lookup table
+const uint8_t semi_sine_wave[] = {128, 162, 194, 220, 239, 250, 250, 239, 220, 194, 162, 128, 93, 61, 35, 16, 5, 5, 16, 35, 61, 93};
+const uint8_t steps = sizeof(semi_sine_wave);
+
 int main() {
     // Set PWM pin as output
     DDRB |= (1 << PWM_PIN);
@@ -18,9 +22,9 @@ int main() {
 
     while(1) {
         // Generate semi-sine wave with a period of 1 ms (1 kHz frequency)
-        for (uint8_t i = 0; i < sizeof(semi_sine_wave); i++) {
+        for (uint8_t i = 0; i < steps; i++) {
             OCR0A = semi_sine_wave[i];
-            _delay_ms(1); // Delay 1 ms for 1 kHz frequency
+            _delay_ms(1.0/steps); // Delay 1/22 ms for 1 kHz frequency
         }
     }
 
